d58aa76cbb005774e64584b8148fee0082bef4dc
Gerardo Ontiveros <gontiverosdavila01@gmail.com>
2025-03-31 18:02:11 -0600

    Configuracion de temas

---
+++ src/theme/DarkTheme.js
@@ -0,0 +1,11 @@
+const DarkTheme = {
+  isDark: true,
+  colors: {
+    primary: "#FF0000",
+    secondary: "#00FF00",
+    background: "#000000",
+    text: "#FFFFFF",
+  },
+};
+
+export default DarkTheme;
---
+++ src/theme/LIghtTheme.js
@@ -0,0 +1,11 @@
+const LightTheme = {
+  isDark: false,
+  colors: {
+    primary: "#FF0000",
+    secondary: "#00FF00",
+    background: "#FFFFFF",
+    text: "#000000",
+  },
+};
+
+export default LightTheme;
