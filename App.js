92cfaf7c12798474c878f794c410585b45af9c10
Gerardo Ontiveros <gontiverosdavila01@gmail.com>
2025-03-31 19:10:32 -0600

    Cambio de nombre de contexto (ThemeContext)

--- App.js
+++ App.js
@@ -1,22 +1,22 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { DarkTheme, LightTheme } from "@react-navigation/native";
 import React, { useMemo, useState } from "react";
-import { AppContext } from "@context/AppContext";
+import { ThemeContext } from "@context/ThemeContext";
 import { TabNavigator } from "@routes/screens.routing";
 import { StatusBar } from "expo-status-bar";
 
 export default function App() {
   const [isDarkTheme, setIsDarkTheme] = useState(false);
 
-  const appContext = useMemo(() => {
+  const themeContext = useMemo(() => {
     return { isDarkTheme, setIsDarkTheme };
   });
   return (
     <NavigationContainer theme={isDarkTheme ? DarkTheme : LightTheme}>
-      <AppContext.Provider value={appContext}>
+      <ThemeContext.Provider value={themeContext}>
         <TabNavigator />
         <StatusBar style={isDarkTheme ? "light" : "dark"} />
-      </AppContext.Provider>
+      </ThemeContext.Provider>
     </NavigationContainer>
   );
 }
