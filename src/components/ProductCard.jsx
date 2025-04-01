4b3a73ddb76706cde549844c034653771dfa22c3
Gerardo Ontiveros <gontiverosdavila01@gmail.com>
2025-03-31 19:08:54 -0600

    Crear componente de producto y darle estilos

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,12 +1,59 @@
-import { View, Text } from "react-native";
 import React from "react";
+import { View, Text, Image, TouchableOpacity, StyleSheet } from "react-native";
 
-const ProductCard = () => {
+export const ProductCard = ({ image, name, price, onAddToCart }) => {
   return (
-    <View>
-      <Text>ProductCard</Text>
+    <View style={styles.card}>
+      <Image source={{ uri: image }} style={styles.productImage} />
+      <Text style={styles.productName}>{name}</Text>
+      <Text style={styles.productPrice}>{price}</Text>
+      <TouchableOpacity
+        style={styles.addButton}
+        onPress={() => onAddToCart(product)}
+      >
+        <Text style={styles.addButtonText}>Agregar al carrito</Text>
+      </TouchableOpacity>
     </View>
   );
 };
 
-export default ProductCard;
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: "#fff",
+    borderRadius: 15,
+    margin: 10,
+    padding: 10,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 5,
+    elevation: 3,
+    width: "90%",
+  },
+  productImage: {
+    width: "100%",
+    height: 120,
+    borderRadius: 10,
+    marginBottom: 10,
+  },
+  productName: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "#333",
+  },
+  productPrice: {
+    fontSize: 14,
+    color: "#666",
+    marginVertical: 5,
+  },
+  addButton: {
+    backgroundColor: "#625dff",
+    paddingVertical: 10,
+    borderRadius: 5,
+    alignItems: "center",
+  },
+  addButtonText: {
+    color: "white",
+    fontWeight: "bold",
+  },
+});
