a32f1eeb7f1f723c3c899695b438df29bcf778c9
Gerardo Ontiveros <gontiverosdavila01@gmail.com>
2025-03-31 19:04:25 -0600

    agregar lista de productos traida desde SUPABASE

--- src/screens/ProductList.jsx
+++ src/screens/ProductList.jsx
@@ -1,12 +1,82 @@
-import { View, Text } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  ActivityIndicator,
+} from "react-native";
+import React, { useState, useEffect } from "react";
+import { supabase } from "@lib/supabase";
+import { ProductCard } from "@components/ProductCard";
 
 const ProductList = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const { data, error } = await supabase.from("productos").select();
+        if (error) {
+          throw error;
+        }
+        setProducts(data);
+      } catch (error) {
+        console.error("Lista vacía");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
+  const renderProductItem = ({ item }) => (
+    <ProductCard
+      id={item.id}
+      name={item.name}
+      price={item.price}
+      image={item.image}
+    />
+  );
+
+  if (loading) {
+    return (
+      <View style={styles.center}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+
   return (
-    <View>
-      <Text>ProductList</Text>
+    <View style={styles.container}>
+      <Text>Lista de Productos</Text>
+      <FlatList
+        data={products}
+        renderItem={renderProductItem}
+        keyExtractor={(item) => item.id.toString()}
+        numColumns={2}
+        columnWrapperStyle={styles.columnWrapper}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+  columnWrapper: {
+    justifyContent: "space-between",
+    paddingHorizontal: 8,
+    flexDirection: "colum",
+  },
+  center: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 
 export default ProductList;
